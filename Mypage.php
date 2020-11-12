1b1e930291cfb8c307e4f1eec3e149d9a493a8b3
fatou <niangfatou040@gmail.com>
2020-11-12 15:57:03 +0000

    creation

---
+++ Mypage.php
@@ -0,0 +1,423 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My page</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" media="screen" href="design.css">
+</head>
+<body>
+<div class="card bg-dark text-white">
+  <img src="images/BackgroundImage.png" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <a class="card-title color1" href="">golden</a>
+    <div class="text-right">
+    <a class="card-title color" href="">Home</a>&nbsp;&nbsp;
+    <a class="card-title color" href="">Services</a>&nbsp;&nbsp;
+    <a class="card-title color" href="">PortFolio</a>&nbsp;&nbsp;
+    <a class="card-title color" href="">About</a>&nbsp;&nbsp;
+    <a class="card-title color" href="">Contact</a>
+    </div>
+    <br><br><br><br>
+    <br><br><br><br>
+	<br><br><br><br>
+	<br><br><br><br>
+    <p class="card-text">
+     <h1 class="text-center">Welcome To Our Studio!</h1>
+
+     <h3 class="text-center">it's nice to meet you</h3>
+     <div class="text-center">
+     <button type="button" class="btn btn-warning bouton">Tell me more</button>
+     </div>
+    </p>
+  </div>
+</div>
+
+
+<div class="container-fluid text-center">
+<article>
+
+	<br>
+  <h3>Services</h3>
+  <p class="sous-titre"><small class="text-muted ">Proin iaculis purus consequat sem cure. </small></p>
+<table align="center">
+	<tr>
+<td>
+  <div class="cont">
+          <img src="images/Circle.png" ><br>
+          <div class="text">
+          <a href=""><img src="images/Icon-Basket.png" class="figure-img"></a>
+          </div>
+</div></td>
+<td>
+<div class="cont">
+          <img src="images/Circle.png" ><br>
+          <div class="text">
+          <a href=""><img src="images/Icon-Laptop.png"></a>
+        </div>
+</div>
+</td>
+<td>
+<div class="cont">
+          <img src="images/Circle.png" ><br>
+          <div class="text">
+          <a href=""><img src="images/Icon-Locked.png"></a>
+        </div>
+</div>
+</td>
+</tr>
+<tr>
+	<td><h5>E-commerce </h5> <br>
+	<p><small class="text-muted ">Proin iaculis purus consequat sem cure &nbsp;&nbsp;&nbsp;&nbsp;<br>
+	Donec porttitora entum suscipit  digni ssim.&nbsp;&nbsp;&nbsp;&nbsp;<br> aenean rhoncus posuere odio in tincidunt.</small></p></td>
+
+	<td><h5>Responsive web</h5><br>
+	<small class="text-muted "><p>Proin iaculis purus consequat sem cure &nbsp;&nbsp;&nbsp;&nbsp;<br>
+	Donec porttitora entum suscipit  digni ssim.&nbsp;&nbsp;&nbsp;&nbsp;<br> aenean rhoncus posuere odio in tincidunt.</p></small></td>	
+	<td><h5>Web security</h5><br>
+	<small class="text-muted "><p>Proin iaculis purus consequat sem cure &nbsp;&nbsp;&nbsp;&nbsp;<br>
+	Donec porttitora entum suscipit  digni ssim. &nbsp;&nbsp;&nbsp;&nbsp;<br> aenean rhoncus posuere odio in tincidunt.</p></small></td>	
+</tr>
+</table>
+</article>
+<br><br>
+<article class="bg-light text-center">
+	<h3>Our Portfolio</h3>
+	<p class="sous-titre"><small class="text-muted ">Proin iaculis purus consequat sem cure. </small></p>
+
+<div class="card" style="width: 18rem; display: inline-block;">
+  <img src="images/Image1.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Ebony & Ivory</h5>
+    <p class="card-text sous-titre"><small class="text-muted ">Branding</small></p>
+</div>
+</div>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<div class="card" style="width: 18rem; display: inline-block;">
+<img src="images/Image2.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Smart Stationary</h5>
+    <p class="card-text sous-titre"><small class="text-muted ">Print design</small></p>
+</div>
+</div>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<div class="card" style="width: 18rem; display: inline-block; ">
+<img src="images/Image3.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Clever Poster</h5>
+    <p class="card-text sous-titre"><small class="text-muted ">Print design</small></p>
+</div>
+</div>
+<br><br>
+<br>
+<div class="card" style="width: 18rem; display: inline-block; ">
+<img src="images/Image4.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Vinul Record</h5>
+    <p class="card-text sous-titre"><small class="text-muted ">Product mock-up</small></p>
+</div>
+</div>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<div class="card" style="width: 18rem; display: inline-block; ">
+<img src="images/Image5.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Treehouse Template</h5>
+    <p class="card-text sous-titre"><small class="text-muted ">Web design</small></p>
+</div>
+</div>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<div class="card" style="width: 18rem; display: inline-block; ">
+<img src="images/Image6.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Burned Logo</h5>
+    <p class="card-text sous-titre"><small class="text-muted ">Branding</small></p>
+</div>
+</div>
+<br><br><br><br>
+</article>
+<br><br>
+<article class="text-center">
+	<h3>About us</h3>
+	<p class="sous-titre"><small class="text-muted">Proin iaculis purus consequat sem cure. </small></p>
+   <table align="center">
+   	<tr>
+   		<td><h5>July 2010 </h5><br>
+   			<h5>Our humble beginnings</h5>
+   			<p ><small class="text-muted ">Proin iaculis purus consequat sem cure.Donec porttitora entum suscipit<br> digni ssim.aenean rhoncus posuere odio in tincidunt.Proin iaculis purus<br> consequat sem cure.Donec porttitora entum suscipit</small></p>
+   		</td>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td>
+   			<img src="images/ImageA.png" alt="....">
+   		</td>
+   	</tr>
+   	<tr>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td><img src="images/th.jpg" alt="...."></td>
+   	</tr>
+   	<tr>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td>
+   			<img src="images/ImageB.png">
+   		</td>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td><h5>January 2011 </h5><br>
+   			<h5>Facing startup Battles</h5>
+   			<p ><small class="text-muted ">Proin iaculis purus consequat sem cure.Donec porttitora entum suscipit<br> digni ssim.aenean rhoncus posuere odio in tincidunt.Proin iaculis purus<br> consequat sem cure.Donec porttitora entum suscipit</small></p></td>
+   	</tr>
+   		<tr>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td><img src="images/th.jpg" alt="...."></td>
+   	</tr>
+   	<tr>
+   		<td><h5>December 2012</h5><br>
+   			<h5>Enter the dark days</h5>
+   			<p ><small class="text-muted ">Proin iaculis purus consequat sem cure.Donec porttitora entum suscipit<br> digni ssim.aenean rhoncus posuere odio in tincidunt.Proin iaculis purus<br> consequat sem cure.Donec porttitora entum suscipit</small></p>
+   		</td>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td>
+   			<img src="images/ImageC.png" alt="....">
+   		</td>
+   	</tr>
+   	<tr>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td><img src="images/th.jpg" alt="...."></td>
+   	</tr>
+   	<tr>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td>
+   			<img src="images/ImageD.png">
+   		</td>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   	<td><h5>February 2014</h5><br>
+   			<h5>Our Triumph</h5>
+   			<p ><small class="text-muted ">Proin iaculis purus consequat sem cure.Donec porttitora entum suscipit<br> digni ssim.aenean rhoncus posuere odio in tincidunt.Proin iaculis purus<br> consequat sem cure.Donec porttitora entum suscipit</small></p>
+   		</td>
+   	</tr>
+   	<tr>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+   		<td><img src="images/th.jpg" alt="...."></td>
+   	</tr>
+   	<tr>
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+   		<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+   		<td>
+   		 <div class="cont">
+          <img src="images/circlejau.png" ><br>
+          <div class="text">
+          <img src="images/ImageE.png" alt="....">
+          </div>
+			</div>
+   			</td>
+   	</tr>
+   </table>
+<br>
+</article>
+
+<br><br>
+<article class="bg-light">
+	<h3>Our Amazing Team</h3>
+	<p class="sous-titre"><small class="text-muted">Proin iaculis purus consequat sem cure. </small></p>
+<table align="center">
+	<tr>
+		<td>
+	<div class="rounder" style="width: 18rem; display:inline-block;">
+           <img src="images/kim.jpg" class="img" alt="...." width="300px" height="300px">
+    </div>
+</td>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td>
+	<div class="rounder" style="width: 18rem; display:inline-block;">
+           <img src="images/rico.jpg" class="img" alt="...." width="300px" height="300px">
+    </div>
+</td>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td>
+	<div class="rounder" style="width: 18rem; display:inline-block;">
+           <img src="images/uku.jpg" class="img" alt="...." width="300px" height="300px">
+    </div>
+</td>
+</tr>
+<tr>
+	<td><h5>Kimberly Thompson</h5></td>
+	<td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+	<td><h5>Rico Massimo</h5></td>
+	<td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+	<td><h5>Uku Mason</h5></td>
+</tr>
+<tr>
+	<td><small class="text-muted">Marketer</small></td>
+	<td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+	<td><small class="text-muted">Coder</small></td>
+	<td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+	<td><small class="text-muted">Graphic Design</small></td>
+</tr>
+ <tr>
+ 	<td><div class="cont">
+          <img src="images/Twitter Background.png" ><br>
+          <div class="text">
+          <img src="images/twitter.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+</div>
+	
+  <div class="cont">
+          <img src="images/Pinterest Background.png" ><br>
+          <div class="text">
+          <img src="images/pinterest.jpg" class="figure-img" width="80%" height="80%">
+          </div>
+</div>
+
+
+<div class="cont">
+          <img src="images/Facebook Background.png" ><br>
+          <div class="text">
+          <img src="images/face.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+      </div></td>
+      <td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+ 	<td><div class="cont">
+          <img src="images/Twitter Background.png" ><br>
+          <div class="text">
+          <img src="images/twitter.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+</div>
+<div class="cont">
+          <img src="images/Facebook Background.png" ><br>
+          <div class="text">
+          <img src="images/face.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+      </div>
+<div class="cont">
+          <img src="images/Google+ Background.png" ><br>
+          <div class="text">
+          <img src="images/g+.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+</div></td>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+ 	<td>
+ 		 <div class="cont">
+          <img src="images/Pinterest Background.png" ><br>
+          <div class="text">
+          <img src="images/pinterest.jpg" class="figure-img" width="80%" height="80%">
+          </div> 
+</div>
+<div class="cont">
+          <img src="images/Facebook Background.png" ><br>
+          <div class="text">
+          <img src="images/face.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+      </div>
+
+ 	</td>
+ </tr> 
+</table>
+<br><br>
+ <p class="sous-titre"><small class="text-muted ">Proin iaculis purus consequat sem cure.Donec porttitora entum suscipit  digni ssim. &nbsp;&nbsp;&nbsp;&nbsp;<br> aenean rhoncus posuere odio in tincidunt.</small></p>
+</article>
+<br><br><br>
+
+<article class="text-center">
+	<img src="images/Envato.png">
+	&nbsp;&nbsp;&nbsp;&nbsp;
+	<img src="images/WordPress.png">
+	&nbsp;&nbsp;&nbsp;&nbsp;
+	<img src="images/Tuts+.png">
+	&nbsp;&nbsp;&nbsp;&nbsp;
+	<img src="images/Microlancer copy.png">
+</article>
+<br><br>
+
+<article class="bgimg">
+	<h3 class="bouton">Contact us</h3>
+	<p class="sous-titre"><small class="text-muted">Proin iaculis purus consequat sem cure. </small></p>
+
+  <div class="footer">
+  <div class="footer-content">
+    <div class="footer-section about">
+ <form>
+
+<div class="row">
+  <div class="col-sm-6">   
+       <div class="col">
+      <input type="text" class="form-control" placeholder="Your name*">
+    </div> <br>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Your e-mail*">
+    </div><br>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Subject*">
+    </div>
+    
+  </div>
+  <div class="col-sm-6">    
+       <div class="mb-3">
+    <textarea class="form-control" id="validationTextarea" placeholder="Your message*"  rows="6" cols="33" required ></textarea>
+  </div>   
+    </div>
+
+</div>
+<br><br>
+ <button class="btn btn-warning bouton" type="submit">Send message</button>
+</form>
+</div>
+ 
+</div>
+ 
+ </div>
+ <br><br>
+</article>
+
+<br><br>
+<footer class="text-center">
+<div class="footer-bottom">
+<small class="text-muted ">&copy;Copyright 2014 FreeBiesXpress.com</small> 
+ &nbsp;&nbsp;&nbsp;&nbsp;
+<div class="cont">
+          <img src="images/Twitter Background.png" >
+          <div class="text">
+          <img src="images/twitter.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+</div>/*-
+	
+  <div class="cont">
+          <img src="images/Pinterest Background.png" >
+          <div class="text">
+          <img src="images/pinterest.jpg" class="figure-img" width="80%" height="80%">
+          </div>
+</div>
+<div class="cont">
+          <img src="images/Google+ Background.png" >
+          <div class="text">
+          <img src="images/g+.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+</div>
+<div class="cont">
+          <img src="images/Facebook Background.png" >
+          <div class="text">
+          <img src="images/face.jpg" class="figure-img" width="105%" height="105%">
+          </div>
+      </div> 
+</div>
+
+</footer>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
